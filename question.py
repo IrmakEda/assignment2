ebd9c54f54c9cd0c0fb93e045603e364157db7bf
Irmak Eda <irmakeda.aykan@ogr.iu.edu.tr>
2026-01-03 16:43:49 +0300

    add brca1 and hox

--- question.py
+++ question.py
@@ -1,22 +1,48 @@
-## Irmak Eda Aykan
+## Irmak Eda Aykan-Student1
 ###foxp2
 import Bio
 from Bio import SeqIO
-
 #for printing the sequence identifiers
 seq_file_path_foxp2 = "protein_seq_folder/foxp2.fasta"
 seq_handle_foxp2 = SeqIO.parse(seq_file_path_foxp2, "fasta")
-
 for seq_record in seq_handle_foxp2:
     print (seq_record.id)
     print (repr(seq_record.seq))
     print (len(seq_record))
-
 #for converting the files to the genbank format
 count = SeqIO.convert(seq_file_path_foxp2, "fasta", "foxp2.genbank", "genbank")
 print ("Converted %i records" % count)
 
-## Alperen Şen
+###brca1
+import Bio
+from Bio import SeqIO
+#for printing the sequence identifiers
+seq_file_path_brca1 = "protein_seq_folder/brca1.fasta"
+seq_handle_brca1 = SeqIO.parse(seq_file_path_brca1, "fasta")
+for seq_record in seq_handle_brca1:
+    print (seq_record.id)
+    print (repr(seq_record.seq))
+    print (len(seq_record))
+#for converting the files to the genbank format
+count = SeqIO.convert(seq_file_path_brca1, "fasta", "brca1.genbank", "genbank")
+print ("Converted %i records" % count)
+
+###hox
+import Bio
+from Bio import SeqIO
+#for printing the sequence identifiers
+seq_file_path_hox = "protein_seq_folder/hox.fasta"
+seq_handle_hox = SeqIO.parse(seq_file_path_hox, "fasta")
+for seq_record in seq_handle_hox:
+    print (seq_record.id)
+    print (repr(seq_record.seq))
+    print (len(seq_record))
+#for converting the files to the genbank format
+count = SeqIO.convert(seq_file_path_hox, "fasta", "hox.genbank", "genbank")
+print ("Converted %i records" % count)
+
+
+## Alperen Şen-Student2
 ###prion
 import Bio
 from Bio import SeqIO
